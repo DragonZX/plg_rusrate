7f34edbeb1e020c70f2faa35526da071e664cb36
DragonZX <DragonZX@yandex.ru>
2015-04-12 12:12:07 +0300

    Spaces

--- rusrate.php
+++ rusrate.php
@@ -49,7 +49,7 @@ class  plgSystemRusRate extends JPlugin {
 		switch($age) {
 			case '0': $text='PLG_SYSTEM_RUSRATE_MESSAGE_ZERO'; break;
 			case '18': case '21': $text='PLG_SYSTEM_RUSRATE_MESSAGE_ADULT'; break;
-			default: $text='RESTRICTED_FOR'.$age.'YEARS_OLD'; break;
+			default: $text='RESTRICTED_FOR '.$age.' YEARS_OLD'; break;
 		}
 
 		// Getting created page text
